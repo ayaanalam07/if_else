636a36b2c8a1a154092847141ba773e9c96c41ec
Ayaan Alam <ayaanabaf@gmail.com>
2024-02-29 22:53:15 +0500

    if else

---
+++ app.js
@@ -0,0 +1,37 @@
+var percentage = +prompt("enter your percentage")
+
+if(percentage  >=91 && percentage <=  100 ){
+    console.log(" A1 grade")
+    
+}
+
+ else if(percentage  >=81 &&  percentage <= 90 ){
+    console.log("A+ grade")
+
+}
+
+ else if(percentage  >=71 &&  percentage <= 80 ){
+    console.log("A grade")
+
+}
+
+ else if(percentage  >=61 &&  percentage <= 70 ){
+    console.log("B grade")
+
+}
+
+ else if(percentage  >=51 &&  percentage <= 60 ){
+    console.log(" grade")
+
+}
+
+
+var num = +prompt("enter your number");
+
+if ( num % 2 === 0 ){
+    console.log("It is divisible by 2")
+}
+
+else{
+    console.log("It is not divisible by 2")
+}
